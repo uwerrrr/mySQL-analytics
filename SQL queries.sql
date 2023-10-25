37c507cdcae679973eb5719f1e36fba44c519fe7
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2023-10-25 12:11:15 +1100

    imported dataset

--- SQL queries.sql
+++ SQL queries.sql
@@ -1,18 +1,14 @@
+SHOW TABLES;
+SELECT * FROM dataset;
+
+DROP TABLE dataset;
+
+
 ## Create database
 CREATE DATABASE ecommerce;
 USE ecommerce;
 
 ## Create a imported dataset table
-CREATE TABLE dataset (
-    InvoiceNo INT,
-    StockCode VARCHAR(255),
-    Description TEXT,
-    Quantity INT,
-    InvoiceDate DATETIME,
-    UnitPrice DECIMAL(10, 2),
-    CustomerID INT,
-    Country VARCHAR(255)
-);
 CREATE TABLE dataset (
     InvoiceNo VARCHAR(255),
     StockCode VARCHAR(255),
@@ -24,20 +20,31 @@ CREATE TABLE dataset (
     Country VARCHAR(255)
 );
 
-SHOW TABLES;
-SELECT * FROM dataset;
-
-DROP TABLE dataset;
 
 ## Import dataset into database as one table
-## The script below does NOT work because of server sercurity option
--- LOAD DATA INFILE '/Users/vannguyen/Library/Mobile Documents/com~apple~CloudDocs/<>iCloud cua Van/Work/Projects/Repos/mySQL-analytics/dataset/data.csv' 
-LOAD DATA INFILE './dataset/data.csv' 
+-- Remember to adjust secure config of mysql server
+LOAD DATA INFILE "/usr/local/mysql-8.1.0-macos13-x86_64/to-import-data/dataset/data.csv" -- directory to dataset csv file
 	INTO TABLE dataset
+    CHARACTER SET latin1
 	FIELDS TERMINATED BY ','
 	ENCLOSED BY '"'
 	LINES TERMINATED BY '\n' 
 	IGNORE 1 LINES; -- skip header row
 
 
-SHOW VARIABLES LIKE 'secure_file_priv';
+## DATABASE NORMALISATION
+### Countries Table
+-- create
+CREATE TABLE Countries (
+    CountryID INT AUTO_INCREMENT PRIMARY KEY,
+    CountryName VARCHAR(255) UNIQUE
+);
+-- populate
+INSERT INTO Countries (CountryName)
+	SELECT DISTINCT Country FROM dataset; 
+-- check
+SELECT * FROM Countries;
+
+
+
+
